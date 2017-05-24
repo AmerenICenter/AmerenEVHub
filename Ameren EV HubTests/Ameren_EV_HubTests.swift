368cf9f20411975ae698823d7bcb95ac668ddf5f
Rick Lewis <ejlewis2@illinois.edu>
2017-05-24 10:49:56 -0500

    first

---
+++ Ameren EV HubTests/Ameren_EV_HubTests.swift
@@ -0,0 +1,36 @@
+//
+//  Ameren_EV_HubTests.swift
+//  Ameren EV HubTests
+//
+//  Created by Rick Lewis on 5/24/17.
+//  Copyright © 2017 Ameren. All rights reserved.
+//
+
+import XCTest
+@testable import Ameren_EV_Hub
+
+class Ameren_EV_HubTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
