73a2ea8ad09de88295c6dd35f02513e37ad3fbb3
AmereniCenter4 <iCenterMac4@AmereniCenter4s-MacBook-Pro.local>
2017-06-05 12:54:59 -0500

    "Faizan's version"

--- Ameren EV Hub/AlertMeViewController.swift
+++ Ameren EV Hub/AlertMeViewController.swift
@@ -14,31 +14,66 @@ import FirebaseDatabase
 //  Allows user to register a car to a car charging port
 //  and alerts them when the car is done charging.
 
-class AlertMeViewController: UIViewController {
+class AlertMeViewController: UIViewController,UIPickerViewDataSource, UITextFieldDelegate, UIPickerViewDelegate {
     
     var ref: FIRDatabaseReference!
-    @IBOutlet weak var EVSEForMe: UITextField!
+//    @IBOutlet weak var EVSEForMe: UITextField!
+    var EVSEForMe: String = ""
     var isInUseFlag = false
     var refHandle : UInt = 0
+    
+////////////////////////////////////////////////////////////////////////
+    
+////////////////////////////////////////////////////////////////////////
+//  This is deleted at last working interval
+    @IBOutlet weak var dropDown: UIPickerView!
+    
+    var names = ["parkingSpot1", "parkingSpot2", "parkingSpot3"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return names.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return names[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        self.EVSEForMe = self.names[row]
+       // self.dropDown.isHidden = true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        self.dropDown.isHidden = false
+    }
+    
+    
+    
+///////////////////////////////////////////////////////////////////////////
+    
     
     //  Function that creates/ends the alert for the user.
     
     @IBAction func addToAlert(_ sender: Any) {
-        if !isInUseFlag && (EVSEForMe.text != "") {
+        if !isInUseFlag && (EVSEForMe != "") {
             isInUseFlag = true
-            refHandle = self.ref.child("EVApp/EVAppData").child(EVSEForMe.text!).observe(FIRDataEventType.value, with: { snap in
+            refHandle = self.ref.child("EVApp/EVAppData").child(EVSEForMe).observe(FIRDataEventType.value, with: { snap in
                 let state = String(describing: snap.childSnapshot(forPath: "EVSEState").valueInExportFormat()!)
                 if (state == "4") {
                     //  If the car has charged then stop checking it and call the helper function.
                     print("Car is done charging please move it!")
-                    self.ref.child("EVApp/EVAppData").child(self.EVSEForMe.text!).removeObserver(withHandle: self.refHandle)
+                    self.ref.child("EVApp/EVAppData").child(self.EVSEForMe).removeObserver(withHandle: self.refHandle)
                     self.carIsCharged()
                 }
                 else {
                     print("Car is still charging!")
                 }
             })
-        } else if EVSEForMe.text == "" {
+        } else if EVSEForMe == "" {
             print("Please enter a valid ID")
         } else {
             onlyOneCar()
@@ -50,7 +85,7 @@ class AlertMeViewController: UIViewController {
     @IBAction func removeCar(_ sender: Any) {
         if isInUseFlag {
             print("Car is removed from alert me system!")
-            self.ref.child("EVApp/EVAppData").child(self.EVSEForMe.text!).removeObserver(withHandle: self.refHandle)
+            self.ref.child("EVApp/EVAppData").child(self.EVSEForMe).removeObserver(withHandle: self.refHandle)
             isInUseFlag = false
         } else {
             print("Nothing interesting happens")
